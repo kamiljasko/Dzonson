052ee784f05ce8025b293c26b86c288ecab03226
kamiljasko <kamiljasko@interia.pl>
2022-05-07 14:21:05 +0200

    Elo

---
+++ main.py
@@ -0,0 +1,5 @@
+def to_jaden_case(string):
+
+if __name__ == '__main__':
+    quote = "How can mirrors be real if our eyes aren't real"
+    assert(to_jaden_case(quote), "How Can Mirrors Be Real If Our Eyes Aren't Real")
