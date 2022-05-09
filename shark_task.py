4bef01efe55bef20ba1050a44d6ffe3da558531b
kamiljasko <kamiljasko@interia.pl>
2022-05-09 20:02:32 +0200

    Elo

--- shark_task.py
+++ shark_task.py
@@ -4,7 +4,7 @@ def shark(pontoon_distance, shark_distance, you_speed, shark_speed, dolphin):
         shark_speed = shark_speed / 2
     sharktime = shark_distance / shark_speed
     if mytime >= sharktime:
-        return "Shark bait!"
+        return "Shark Bait!"
     else:
         return "Alive!"
 
